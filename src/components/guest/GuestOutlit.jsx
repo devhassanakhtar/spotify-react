1d94c73c5fbaeacccf49a93801f4d426a0923370
devhassanakhtar <hcodex5@gmail.com>
2026-05-20 17:09:13 +0500

    fix layout issue

--- src/components/guest/GuestOutlit.jsx
+++ src/components/guest/GuestOutlit.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GuestOutlit = () => {
   return (
-    <div className='h-screen rounded-lg  bg-[var(--primary-bg)] p-3'>
+    <div className='h-full rounded-lg  bg-[var(--primary-bg)] p-3'>
       
     </div>
   )
